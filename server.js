772cf290d0fae276cc2f8b22b049a2fe7c421d45
Alissa Sobo <alissasobo@gmail.com>
2018-01-02 18:15:30 -0800

    add search for businesses endpoint

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const apiServer = require('./api-server');
 const express = require('express');
 const database = require("./src/database");
+const bodyParser = require('body-parser');
 const expressLayout = require('express-ejs-layouts');
+const search = require("./src/search")
 
 const port = 8000;
 
@@ -19,10 +21,31 @@ app.set('views', 'src/views')
 app.use(expressLayout);
 app.use('/static', express.static('static'));
 app.use('/api', apiServer);
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.render('index', {currentTime: Date.now()})
-})
+});
+
+app.get('/searchforbusinesses', async function(req, res) {
+  const category = req.query.category;
+  const location = req.query.location;
+  const latitude = 45.5231;
+  const longitude = -122.6765;
+
+  const searchResults = await search.searchForBusinesses(category, latitude, longitude);
+
+
+  res.render('search_results',
+    {
+      category: category,
+      location: location,
+      businesses: searchResults
+    }
+  );
+
+});
 
 app.listen(port, function onStart(err) {
   if (err) {
