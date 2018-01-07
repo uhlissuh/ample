ad745c0b3495edc7d4cea110c1469acd7eae63e1
Alissa Sobo <alissasobo@gmail.com>
2018-01-06 17:42:36 -0800

    Add method for autocompleting place names w/ google places API
    Also, move google places methods into a class: GooglePlacesClient

--- src/google-places.js
+++ src/google-places.js
@@ -1,46 +1,61 @@
-const request = require('request-promise');
 const GOOGLE_URL = 'https://maps.googleapis.com/maps/api'
 const {GOOGLE_API_KEY} = process.env
 
-exports.getBusinessesNearCoordinates = async function (term, latitude, longitude) {
-  const response = await request({
-    uri: `${GOOGLE_URL}/place/nearbysearch/json?` +
-         `key=${GOOGLE_API_KEY}&` +
-         `location=${latitude},${longitude}&` +
-         `radius=50000&` +
-         `keyword=${encodeURIComponent(term)}`,
-    json: true
-  });
-
-  if (response.status != 'OK') {
-    throw new Error(`Google place search failed. Status: ${response.status}, ${JSON.stringify(response)}`);
+module.exports =
+class GooglePlacesClient {
+  constructor(request) {
+    this.request = request || require('request-promise');
   }
 
-  return response.results;
-}
+  async getBusinessesNearCoordinates(term, latitude, longitude) {
+    const response = await this.request({
+      uri: `${GOOGLE_URL}/place/nearbysearch/json?` +
+           `key=${GOOGLE_API_KEY}&` +
+           `location=${latitude},${longitude}&` +
+           `radius=50000&` +
+           `keyword=${encodeURIComponent(term)}`,
+      json: true
+    });
 
-exports.getBusinessById = async function(id) {
-  const response = await request({
-    uri: `${GOOGLE_URL}/place/details/json?key=${GOOGLE_API_KEY}&placeid=${id}`,
-    json: true
-  });
+    if (response.status != 'OK') {
+      throw new Error(`Google place search failed. Status: ${response.status}, ${JSON.stringify(response)}`);
+    }
 
-  if (response.status != 'OK') {
-    throw new Error(`Google place details failed. Status: ${response.status}, ${JSON.stringify(response)}`);
+    return response.results;
   }
 
-  return response.result;
-}
+  async getBusinessById(id) {
+    const response = await this.request({
+      uri: `${GOOGLE_URL}/place/details/json?key=${GOOGLE_API_KEY}&placeid=${id}`,
+      json: true
+    });
 
-exports.getCoordinatesForLocationName = async function(locationName) {
-  const response = await request({
-    uri: `${GOOGLE_URL}/geocode/json?key=${GOOGLE_API_KEY}&address=${encodeURIComponent(locationName)}`,
-    json: true
-  });
+    if (response.status != 'OK') {
+      throw new Error(`Google place details failed. Status: ${response.status}, ${JSON.stringify(response)}`);
+    }
 
-  if (response.status != 'OK') {
-    throw new Error(`Google geocoding failed. Status: ${response.status}`);
+    return response.result;
   }
 
-  return response.results[0].geometry.location;
-}
+  async getCoordinatesForLocationName(locationName) {
+    const response = await this.request({
+      uri: `${GOOGLE_URL}/geocode/json?key=${GOOGLE_API_KEY}&address=${encodeURIComponent(locationName)}`,
+      json: true
+    });
+
+    if (response.status != 'OK') {
+      throw new Error(`Google geocoding failed. Status: ${response.status}`);
+    }
+
+    return response.results[0].geometry.location;
+  }
+
+  async autocompletePlaceName(input) {
+    const response = await this.request({
+      uri: `${GOOGLE_URL}/place/autocomplete/json?key=${GOOGLE_API_KEY}&types=geocode&input=${encodeURIComponent(input)}`,
+      json: true
+    });
+
+    return response.predictions.map(prediction => prediction.description)
+  }
+};
---
+++ test/google-places-test.js
@@ -0,0 +1,21 @@
+const GooglePlacesClient = require('../src/google-places');
+const assert = require('assert');
+
+describe("google places", () => {
+  describe(".autocompletePlaceName", () => {
+    it("autocompletes the place name using the google places API", async () => {
+      const client = new GooglePlacesClient(async () => {
+        return require('./fixtures/google-places-autocomplete-response.json');
+      });
+
+      const results = await client.autocompletePlaceName('Alberta');
+      assert.deepEqual(results, [
+        'Alberta, Portland, OR, United States',
+        'North Alberta Street, Portland, OR, United States',
+        'Northeast Alberta Street, Portland, OR, United States',
+        'Alberta Park Trail, Portland, OR, United States',
+        'Alberta, Canada'
+      ]);
+    });
+  });
+});
