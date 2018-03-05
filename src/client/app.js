1f229486d9a2f0ac3616ee5d26931a65cce114a4
Alissa Sobo <alissasobo@gmail.com>
2018-03-04 21:54:51 -0800

    Set up webpack

---
+++ src/client/app.js
@@ -0,0 +1,1 @@
+const StarView = require('./star-view');
---
+++ webpack.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  entry: './src/client/app',
+  output: {
+    path: `${__dirname}/static`,
+    filename: 'bundle.js'
+  }
+};
