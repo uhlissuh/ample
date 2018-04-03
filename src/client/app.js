00829ba31ebfe67713bf585fe9213028b10fb6f8
Alissa Sobo <alissasobo@gmail.com>
2018-04-03 12:25:29 -0700

    Use webpack for concatenating CSS files

--- src/client/app.js
+++ src/client/app.js
@@ -1,3 +1,7 @@
+require('../css/app.css');
+require('../css/autocomplete.css');
+require('../css/star-rating.css');
+
 ample = {
   setUpStarRatings: require('./star-rating-view'),
   collapseNoAnimate: require('./collapse-no-animate'),
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,22 @@
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
 module.exports = {
   entry: './src/client/app',
   output: {
     path: `${__dirname}/static`,
     filename: 'bundle.js'
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          use: "css-loader"
+        })
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin("bundle.css"),
+  ]
 };
